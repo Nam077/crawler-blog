7120464ee9ce3e727465f277cce4e0c1ffce4c66
nam077 <nam077.me@gmail.com>
2023-11-22 20:52:43 +0700

    update code

--- src/dji.ts
+++ src/dji.ts
@@ -6,6 +6,15 @@ function capitalizeFirstLetter(text: string) {
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
 
+const chunk = <T>(array: T[], size: number) => {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+};
+
+
 function getSlugFromUrl(url: string) {
     const urlObject = new URL(url);
     const pathName = urlObject.pathname;
@@ -35,8 +44,7 @@ interface Blog {
 
 }
 
-class TheMeWPCrawler {
-    private readonly URL = "https://themewp.vn/blogs/";
+class DJICrawler {
     private readonly headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
     };
@@ -46,18 +54,30 @@ class TheMeWPCrawler {
     }
 
     public async getSoup(url: string) {
-        const response = await axios.get(url, {headers: this.headers});
-        return cheerio.load(response.data);
+        let retryCount = 3;
+        while (retryCount > 0) {
+            try {
+                const response = await axios.get(url, {headers: this.headers});
+                return cheerio.load(response.data);
+            } catch (e) {
+                retryCount--;
+                console.log(`Retry ${retryCount} time(s) url: ${url}`);
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            }
+        }
+        return cheerio.load('');
     }
 
     public async getAllPosts(url: string) {
         const posts: Blog[] = [];
         const soup = await this.getSoup(url)
         soup("div.col.post-item").each((_index: any, element: any) => {
+            fs.writeFileSync("test.html", soup(element).html() || "");
             let title = soup(element).find("h5.post-title.is-large").text().replaceAll('\n', '').trim();
-            let url = soup(element).find("h5.post-title.is-large a").attr("href") || "";
-
-            let thumbnail = soup(element).find("img.wp-post-image").attr("data-src") || "";
+            // <div class="col-inner">
+            //     <a href="https://dji-vietnam.vn/dji-mini-4-pro-va-dji-mini-3-pro/" class="plain">
+            let url = soup(element).find("a.plain").attr("href") || "";
+            let thumbnail = soup(element).find("img.image-cover").attr("data-src") || "";
             thumbnail = `https:${thumbnail}`;
             posts.push({
                 thumbnail,
@@ -70,33 +90,23 @@ class TheMeWPCrawler {
         return posts;
     }
 
+    public async getAllPage(url: string) {
+        const soup = await this.getSoup(url)
+        return soup("a.page-number").not('a.next').last().text();
+    }
+
     public async getInformation(post: Blog) {
         const soup = await this.getSoup(post.url);
-        let content = soup("div.entry-content");
-        content.find('.xem-them').remove();
-        content.find('.kk-star-ratings').remove();
-        content.find('img').each((_index, element) => {
-            const imgElement = soup(element);
-            imgElement.attr('src', imgElement.attr('data-src'));
-        });
+        let content = soup("div.article-inner");
         post.content = (content.html() || "").replaceAll('\n', '').replaceAll('\t', '').trim();
-        const categoriesHtml = soup('div.danh-muc >a')
+
+        const categoriesHtml = soup('h6.entry-category.is-xsmall > a')
         categoriesHtml.each((_index: any, element: any) => {
             const name = soup(element).text().trim();
             const url = soup(element).attr("href") || "";
             post.categories = post.categories || [];
             post.categories.push({
                 name: capitalizeFirstLetter(name),
-                slug: getSlugFromUrl(url)
-            });
-        });
-        const tagsHtml = soup('div.the-tim-kiem >a')
-        tagsHtml.each((_index: any, element: any) => {
-            const name = soup(element).text().trim();
-            const url = soup(element).attr("href") || "";
-            post.tags = post.tags || [];
-            post.tags.push({
-                name,
                 slug: getSlugFromUrl(url)
             });
         });
@@ -115,13 +125,17 @@ class TheMeWPCrawler {
     if (!fs.existsSync(folderData)) {
         fs.mkdirSync(folderData);
     }
-    const crawler = new TheMeWPCrawler();
-    const HOST = "https://themewp.vn/blogs/page/";
-    const urls = Array.from({length: 7}, (_, i) => `${HOST}${i + 1}`);
-    const promises = urls.map(url => crawler.getAllInformation(url));
-    const results = await Promise.all(promises);
-    fs.writeFileSync(folderData+'/themewp.json', JSON.stringify(results.flat(), null, 2), 'utf-8');
-    // const postContent = fs.readFileSync('posts.json', 'utf-8');
-    // const posts = JSON.parse(postContent) as Blog[];
-    // console.log(posts.length);
+    const type = ['flycam', 'fpv', 'meo-va-huong-dan']
+    const crawler = new DJICrawler();
+    for (const item of type) {
+        const HOST = `https://dji-vietnam.vn/${item}`;
+        const allPages = await crawler.getAllPage(HOST);
+        const urls = Array.from({length: parseInt(allPages)}, (_, i) => `${HOST}/page/${i + 1}`);
+        const results = []
+        for (const url of urls) {
+            results.push(await crawler.getAllInformation(url));
+        }
+        fs.writeFileSync(`${folderData}/${item}.json`, JSON.stringify(results.flat(), null, 2), 'utf-8');
+    }
+
 })();
